58df4c6f6d75ca254ffe1fd58c6d9f3ac7571fc2
Jean Lima <110063438+jeancarlosde-lima@users.noreply.github.com>
2024-03-31 09:05:58 -0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,16 @@
+# Função para desenhar um ovo de Páscoa
+def desenhar_ovo():
+    print("  ____")
+    print(" /    \\")
+    print("/      \\")
+    print("\\      /")
+    print(" \\____/")
+    print("  ||||")
+
+# Função para exibir uma mensagem de Páscoa
+def mensagem_pascoa():
+    print("Feliz Páscoa! Que este dia seja repleto de alegria e renovação.")
+
+# Chamada das funções para desenhar um ovo e exibir a mensagem
+desenhar_ovo()
+mensagem_pascoa()
